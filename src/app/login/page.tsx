0a5e9ba6c706812d9cb4987934c9c57867287999
rhenoresky <rheno2002@gmail.com>
2024-01-26 14:49:28 +0700

    add page login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Login() {
+  const [account, setAccount] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInputChange = (event: {
+    target: { name: string; value: string };
+  }) => {
+    const { name, value } = event.target;
+
+    setAccount({
+      ...account,
+      [name]: value,
+    });
+  };
+
+  const login = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(account),
+    });
+    const res = await response.json();
+    console.log(res);
+  };
+
+  return (
+    <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+      <h1 className="text-4xl font-medium">Login</h1>
+      <p className="text-slate-500">Hi, Welcome back 👋</p>
+
+      <div className="my-5">
+        <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+          <Image
+            src="https://www.svgrepo.com/show/355037/google.svg"
+            className="w-6 h-6"
+            alt="Icon"
+            width={500}
+            height={500}
+          />
+          <span>Login with Google</span>
+        </button>
+      </div>
+      <form className="my-10">
+        <div className="flex flex-col space-y-5">
+          <label htmlFor="email">
+            <p className="font-medium text-slate-700 pb-2">Email address</p>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+              placeholder="Enter email address"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label htmlFor="password">
+            <p className="font-medium text-slate-700 pb-2">Password</p>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+              placeholder="Enter your password"
+              onChange={handleInputChange}
+            />
+          </label>
+          <button
+            onClick={login}
+            className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+              />
+            </svg>
+            <span>Login</span>
+          </button>
+          <p className="text-center">
+            Not registered yet?&nbsp;
+            <a
+              href="#"
+              className="text-indigo-600 font-medium inline-flex space-x-1 items-center"
+            >
+              <span>Register now</span>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </span>
+            </a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
