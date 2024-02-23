46f90aa7a455ca50a3519f29eed6ccbf729e009c
rhenoresky <rheno2002@gmail.com>
2024-02-23 22:38:09 +0700

    store component

--- src/components/storeComponent/StoreComponent.jsx
+++ src/components/storeComponent/StoreComponent.jsx
@@ -1,16 +1,23 @@
 "use client";
 
-import { addBy, decremented, incremented } from "@/lib/store/counterStore";
+import { addBy, decremented, incremented, incrementedAsync } from "@/lib/store/counterStore";
 import { useDispatch, useSelector } from "react-redux";
 
 const StoreComponent = () => {
-  const state = useSelector((state) => state.counterSlice.counter);
+  const counter = useSelector((state) => state.counterSlice.counter);
+  const todo = useSelector((state) => state.counterSlice.todo);
   const dispath = useDispatch();
   return (
     <div>
-      <h1 className="text-white text-2xl">{state}</h1>
+      <h1 className="text-white text-2xl">{counter}</h1>
+      {todo.map((todo) => (
+        <div className="text-white" key={todo.userId}>
+          <h2>{todo.id}</h2>
+          <h2>{todo.title}</h2>
+        </div>
+      ))}
       <div className="flex gap-4">
-        <button className="py-3 px-4 bg-blue-400 text-white" onClick={() => dispath(incremented())}>
+        <button className="py-3 px-4 bg-blue-400 text-white" onClick={() => dispath(incrementedAsync())}>
           increment
         </button>
         <button className="py-3 px-4 bg-blue-400 text-white" onClick={() => dispath(decremented())}>
