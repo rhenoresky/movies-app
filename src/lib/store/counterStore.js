48346d31f4d2873f97b4cda533c4fab43ab9d125
rhenoresky <rheno2002@gmail.com>
2024-03-01 22:51:15 +0700

    add store

--- src/lib/store/counterStore.js
+++ src/lib/store/counterStore.js
@@ -1,9 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
   name: "counter",
   initialState: {
     counter: 0,
+    todo: [],
   },
   reducers: {
     incremented: (state, action) => {
@@ -16,6 +17,17 @@ export const counterSlice = createSlice({
       state.counter += 10;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(incrementedAsync.fulfilled, (state, action) => {
+      state.todo.push(action.payload);
+    });
+  },
+});
+
+export const incrementedAsync = createAsyncThunk("counter/incrementedAsync", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const todo = await response.json();
+  return todo;
 });
 
 export const { incremented, decremented, addBy } = counterSlice.actions;
